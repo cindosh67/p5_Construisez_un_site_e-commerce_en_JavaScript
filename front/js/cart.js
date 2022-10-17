739351e7073d3125d64d22f1e403d495cbbd1337
cindy <r.cindy67@gmail.com>
2022-10-17 21:28:35 +0200

    revision avec mathieu

--- front/js/cart.js
+++ front/js/cart.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     function displayBasket(listProduct) {
         
-        // on récupère le panier converti
+        // on récupère le panier
          let basketStorage = JSON.parse(localStorage.getItem("basket"));
        // si il y a un panier null ou panier < 1
         if (basketStorage === null || basketStorage.length <= 1){
@@ -41,15 +41,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
                         detailProduct.image = listProduct[i].imageUrl;
                         detailProduct.description = listProduct[i].description;
                         detailProduct.alt = listProduct[i].altTxt;
-                    }
-                }
-                document.querySelector(".cart__price p").innerHTML = "Total ( articles) : €"
-            }
+                    };
+                };
+               
+            };
         
         display(basketStorage);
-        totalQuantity(basketStorage);
+        totalQuantity(basketStorage)
+        modifyQuantity();
         };
-    }
+        
+        
+    };
     
     //Fonction d'affichage d'un panier (tableau)
 
@@ -58,7 +61,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         let zonePanier = document.querySelector("#cart__items");
         // on créait les affichages des produits du panier via un map et introduction de dataset dans le code
         zonePanier.innerHTML += prod.map((detailProduct) => 
-        `<article class="cart__item" data-id="${detailProduct._id}" data-color="${detailProduct.color}">
+        `<article class="cart__item" data-id="${detailProduct.id}" data-color="${detailProduct.colors}" data-quantité="${detailProduct.quantity}" data-prix="${detailProduct.price}">
                 <div class="cart__item__img">
                   <img src="${detailProduct.image}" alt="${detailProduct.alt}">
                 </div>
@@ -66,7 +69,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                   <div class="cart__item__content__description">
                     <h2>${detailProduct.name}</h2>
                     <p>${detailProduct.colors}</p>
-                    <p>${detailProduct.price}</p>
+                    <p>${detailProduct.price} €</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
@@ -79,29 +82,67 @@ document.addEventListener("DOMContentLoaded", (event) => {
                   </div>
                 </div>
               </article>`
-        ) 
-        //on remplace les virgules de jonctions des objets du tableau par un vide
+        ).join("") ;
         
-    }
+        ;
+    };
     
     //Fonction quantity total et price total
 
-    function totalQuantity(basketStorage) {
-        
+    function totalQuantity(basketStorage) {  
+
         let editPrice = basketStorage.map((element) => element.price);
 
         let totalPrice = editPrice.reduce((previousValue, currentValue) => previousValue + currentValue);
         console.log(totalPrice);
+
         let editQuantity = basketStorage.map((element) => element.quantity);
         
         let totalQuantity =editQuantity.reduce((previousValue, currentValue) => previousValue + currentValue)
         console.log(totalQuantity);
 
-        if(totalQuantity <= 1){
-            document.querySelector(".cart__price").innerHTML = ` <p>Total ( ${totalQuantity} article) : <span id="totalPrice"><${totalPrice}></span> €</p>`
+        if(totalQuantity === 1 || totalQuantity === 0){
+            document.querySelector(".cart__price p").innerHTML = ` <p>Total ( ${totalQuantity} article) : <span id="totalPrice"><${totalPrice}></span> €</p>`
         }else{
-             document.querySelector(".cart__price").innerHTML = ` <p>Total ( ${totalQuantity} articles) : <span id="totalPrice">${totalPrice}</span> €</p>`
-        } 
+             document.querySelector(".cart__price p").innerHTML = ` <p>Total ( ${totalQuantity} articles) : <span id="totalPrice">${totalPrice}</span> €</p>`
+        } ;
+
+
+
+    };
+    // //Fonction modif 
+
+    function modifyQuantity() {
+      const cart = document.querySelectorAll(".cart__item");
+      /* comment regarde ce qu'onva afficher avec le data*/
+      /* cart.forEach((cart) => {console.log("item panier en dataset: " + " " + cart.dataset.id + " " + cart.dataset.color + " " + cart.dataset.quantité); });*/
+      // On écoute ce qu'il se passe dans itemQuantity de l'article concerné
+      cart.forEach((cart) => {
+        cart.addEventListener("change", (eq) => {
+          console.log(cart.dataset.id);
+          // vérification d'information de la valeur du clic et son positionnement dans les articles
+          let basketStorage = JSON.parse(localStorage.getItem("basket"));
+          // boucle pour modifier la quantité du produit du panier grace à la nouvelle valeur
+          for (article of basketStorage)
+          console.log(article.quantity);
+            if (
+              article.id === cart.dataset.id &&
+              cart.dataset.color === article.colors
+            ) {
+              article.quantity = eq.target.value;
+              
+              localStorage.basket = JSON.stringify(basketStorage);
+              // on met à jour le dataset quantité
+              cart.dataset.quantity = eq.target.value;
+              // on joue la fonction pour actualiser les données
+              totalQuantity(basketStorage);
+              console.log(totalQuantity);
+            }
+        });
+      });
+    }
+    function name(params) {
+      
     }
 });
 
