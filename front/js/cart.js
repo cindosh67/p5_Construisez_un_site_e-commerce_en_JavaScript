2b6287addce69f5ae7992be354bf00f4b2f38cb6
cindy <r.cindy67@gmail.com>
2022-10-17 11:38:40 +0200

    affichage panier + prix total et quantité totam

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,92 +1,107 @@
+// pour différancier la page confirmation et panier
 document.addEventListener("DOMContentLoaded", (event) => {
 
+// Récupération des produits de l'api
+
+// appel de la ressource api product 
+
+    fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((listProduct) => {
+
+        // appel de la fonction affichagePanier
+        displayBasket(listProduct);
+    })
+    .catch((err) => {
+        document.querySelector("#cartAndFormContainer").innerHTML = "<h1>erreur 404</h1>";
+        console.log("erreur 404, sur ressource api: " + err);
+    });
     
-    // Variable qui récupère les articles du panier dans le local storage
-    let basketStorage = JSON.parse(localStorage.getItem("basket"));
-    //console.log(basketStorage);
 
-    //addPrice();
-    //addQuantFunction();
-let basket = [];
-
-
-    //si le Local est vide afficher panier vide
-    if (basketStorage === null || basketStorage.length ===0) {
-        document.querySelector("h1").textContent = ("Votre panier est vide");
+    function displayBasket(listProduct) {
+        
+        // on récupère le panier converti
+         let basketStorage = JSON.parse(localStorage.getItem("basket"));
+       // si il y a un panier null ou panier < 1
+        if (basketStorage === null || basketStorage.length <= 1){
+            // si il n'y a pas de panier on créait un H1 informatif et quantité appropriées
+            document.querySelector("h1").innerHTML =
+                "Vous n'avez pas d'article dans votre panier";
+        }else{
+        // zone clef/valeur de l'api et du panier avec id produit choisit dans le localStorage
+       
+            for (var detailProduct of basketStorage) {
+               
+                for ( i = 0; i < listProduct.length; i++) {
+                   
+                    if (detailProduct.id === listProduct[i]._id) {
+                        // création et ajout de valeurs à panier qui vont servir pour les valeurs dataset
+                        detailProduct.name = listProduct[i].name;
+                        detailProduct.price = listProduct[i].price;
+                        detailProduct.image = listProduct[i].imageUrl;
+                        detailProduct.description = listProduct[i].description;
+                        detailProduct.alt = listProduct[i].altTxt;
+                    }
+                }
+                document.querySelector(".cart__price p").innerHTML = "Total ( articles) : €"
+            }
+        
+        display(basketStorage);
+        totalQuantity(basketStorage);
+        };
     }
-        for (let product of basketStorage){
-            //console.log(product);
+    
+    //Fonction d'affichage d'un panier (tableau)
 
-            let productId = product.id;
-
-                fetch('http://localhost:3000/api/products/'+ productId)
-                .then((data) =>data.json().then((listProducts) => {
-                    var detailProduct = listProducts;
-                    //console.log(detailProduct);
-                
-
-                    // if(product.colors === detailProduct.colors && product.id  === detailProduct._id){
-                    //     basket.push(detailProduct);
-                    //     localStorage.setItem("basket", JSON.stringify(basketStorage))
-                        
-                    // }
-                    // console.log(basket);
-                    
-                    displayCart(detailProduct);
-                    
-        }));
-
-        function displayCart(detailProduct) {
-            document.querySelector("#cart__items").innerHTML +=
-            `
-            <article class="cart__item" data-id="${productId}" data-color="${product.colors}">
+    function display(prod) {
+        // on déclare et on pointe la zone d'affichage
+        let zonePanier = document.querySelector("#cart__items");
+        // on créait les affichages des produits du panier via un map et introduction de dataset dans le code
+        zonePanier.innerHTML += prod.map((detailProduct) => 
+        `<article class="cart__item" data-id="${detailProduct._id}" data-color="${detailProduct.color}">
                 <div class="cart__item__img">
-                    <img src="${detailProduct.imageUrl}" alt="${detailProduct.altTxt}">
+                  <img src="${detailProduct.image}" alt="${detailProduct.alt}">
                 </div>
                 <div class="cart__item__content">
-                    <div class="cart__item__content__description">
-                        <h2>${detailProduct.name}</h2>
-                        <p>${product.colors}</p>
-                        <p>${detailProduct.price}€</p>
+                  <div class="cart__item__content__description">
+                    <h2>${detailProduct.name}</h2>
+                    <p>${detailProduct.colors}</p>
+                    <p>${detailProduct.price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${detailProduct.quantity}">
                     </div>
-                    <div class="cart__item__content__settings">
-                        <div class="cart__item__content__settings__quantity">
-                            <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                        </div>
-                        <div class="cart__item__content__settings__delete">
-                            <p class="deleteItem">Supprimer</p>
-                        </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
                     </div>
+                  </div>
                 </div>
-            </article>
-            `
-            basket.push(detailProduct)
-            
-        };
-        // localStorage.setItem("basket", JSON.stringify(basket));
-          console.log(basket);
+              </article>`
+        ) 
+        //on remplace les virgules de jonctions des objets du tableau par un vide
         
-    };
-        /********************** AJUSTE ARTICLE QUANTITY + PRIX TOTAL **********************/
+    }
     
-    // function addQuantFunction() {
+    //Fonction quantity total et price total
 
-    //     let found2 = basketStorage.map((element) => element.quantity);
-    //     //console.log(found2);
-      
-    //     const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    //     let quant = found2.reduce(reducer);
-    //     console.log(quant);
-    //     if(quant <= 1){
-    //         document.querySelector(".cart__price").innerHTML = ` <p>Total ( ${quant} article) : <span id="totalPrice"><!-- 84,00 --></span> €</p>`
-    //     }else{
-    //         document.querySelector(".cart__price").innerHTML = ` <p>Total ( ${quant} articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>`
-    //     }
+    function totalQuantity(basketStorage) {
         
+        let editPrice = basketStorage.map((element) => element.price);
 
-    //   };
+        let totalPrice = editPrice.reduce((previousValue, currentValue) => previousValue + currentValue);
+        console.log(totalPrice);
+        let editQuantity = basketStorage.map((element) => element.quantity);
+        
+        let totalQuantity =editQuantity.reduce((previousValue, currentValue) => previousValue + currentValue)
+        console.log(totalQuantity);
 
-     
-
+        if(totalQuantity <= 1){
+            document.querySelector(".cart__price").innerHTML = ` <p>Total ( ${totalQuantity} article) : <span id="totalPrice"><${totalPrice}></span> €</p>`
+        }else{
+             document.querySelector(".cart__price").innerHTML = ` <p>Total ( ${totalQuantity} articles) : <span id="totalPrice">${totalPrice}</span> €</p>`
+        } 
+    }
 });
+
